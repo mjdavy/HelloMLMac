725a5104244a5fe418e27b6008ed89cad33aee0c
Martin Davy <mjdavy@hotmail.com>
2018-08-07 10:56:10 -0400

    Initial Commit

---
+++ HelloMLMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloMLMac
+//
+//  Created by Martin Davy on 8/7/18.
+//  Copyright © 2018 Martin Davy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloMLMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloMLMac
+//
+//  Created by Martin Davy on 8/7/18.
+//  Copyright © 2018 Martin Davy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
