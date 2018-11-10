78eeac9822ed4958bf23796255a9dfe9b28dc535
Martin Davy <mjdavy@hotmail.com>
2018-11-10 12:21:06 -0500

    init weights and biases

--- HelloMLMac/Network.swift
+++ HelloMLMac/Network.swift
@@ -12,24 +12,38 @@ public class Network {
     
     let layers: Int
     let sizes: [Int]
-    var biases: [[Float]]
-    var weights: [[[Float]]]
+    var biases = [[Float]]()
+    var weights = [[[Float]]]()
     
     init(sizes: [Int]) {
         
         self.layers = sizes.count
         self.sizes = sizes
-        self.biases = [[Float]]()
-        self.weights = [[[Float]]]()
         
-        for i in 0..<self.layers-1 {
-            self.biases.append([Float](repeating: 0.0, count: self.sizes[i]))
-            self.weights.append([[Float]]())
-            for j in 0 ..< self.sizes[i] {
-                self.biases[i][j] = Float.random(in:0.0...1.0)
-                self.weights[i][j] = [Float]()
-            }
+        for i in 1..<self.layers {
+            biases.append(initBiases(size: sizes[i]))
+            weights.append(initWeights(dims: (sizes[i-1], sizes[i])))
         }
     }
     
+    func sigmoid(value: Double) -> Double {
+        return 1.0 / (1.0 + pow(M_E, -value))
+    }
+    
+    func initWeights(dims: (Int, Int)) -> [[Float]] {
+        
+        var weights = [[Float]]()
+        for _ in 1...dims.1 {
+            let row = (1...dims.0).map {_ in Float.random(in: 0.0...1.0)}
+            weights.append(row)
+        }
+        return weights
+    }
+    
+    func initBiases(size: Int) -> [Float] {
+      
+        let bias = (1...size).map {_ in Float.random(in: 0.0...1.0)}
+        
+        return bias
+    }
 }
--- HelloMLMacTests/HelloMLMacTests.swift
+++ HelloMLMacTests/HelloMLMacTests.swift
@@ -32,9 +32,34 @@ class HelloMLMacTests: XCTestCase {
     
     func testBiasDims() {
          let n = Network(sizes: [2,3,4])
+        
          XCTAssert(n.layers == 3)
+        
          XCTAssert(n.biases.count == 2)
+         XCTAssert(n.biases[0].count == 3)
+         XCTAssert(n.biases[1].count == 4)
+        
          XCTAssert(n.weights.count == 2)
+         XCTAssert(n.weights[0].count == 3)
+         XCTAssert(n.weights[1].count == 4)
+        
+         XCTAssert(n.weights[0][0].count == 2)
+         XCTAssert(n.weights[0][1].count == 2)
+         XCTAssert(n.weights[0][2].count == 2)
+        
+         XCTAssert(n.weights[1][0].count == 3)
+         XCTAssert(n.weights[1][1].count == 3)
+         XCTAssert(n.weights[1][2].count == 3)
+         XCTAssert(n.weights[1][3].count == 3)
+    }
+    
+    func testSigmoid() {
+        let n = Network(sizes: [0,0,0])
+        for i in -10...10 {
+            let x = n.sigmoid(value: Double(i))
+            XCTAssert(x >= 0.0)
+            XCTAssert(x <= 1.0)
+        }
     }
     
     func testPerformanceExample() {
